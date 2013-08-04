c1c99593e6df50e1135fa0e693bd405156d2070d
Patrick Smith <pjs482+mygithub@gmail.com>
2013-08-04 09:55:35 -0400

    setup: Make big_red installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='big_red',
+    version='0.1',
+    description='Python handler for a Big Red Button.',
+    author='Patrick Smith',
+    author_email='pjs482@gmail.com',
+    packages=['big_red'],
+)
