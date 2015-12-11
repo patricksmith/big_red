94aa77f47f759e3eed777aec5e088578c8314b20
Patrick Smith <patrick@promptworks.com>
2015-12-11 10:46:25 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name='big_red',
-    version='0.1',
+    version='0.2',
     description='Python handler for a Big Red Button.',
     author='Patrick Smith',
     author_email='pjs482@gmail.com',
